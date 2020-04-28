2bb1d2009a056d67427e119113b79e0946685f2c
David Hong <hsurf22@gmail.com>
2020-04-27 20:29:23 -0700

    Wallet > Sidemenu - Replace vuetify elements with custom elements

--- src/web/views/WalletView/components/SideMenu/components/AccordionMenu/AccordionMenu.vue
+++ src/web/views/WalletView/components/SideMenu/components/AccordionMenu/AccordionMenu.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="mew-component--accordion-menu-container user-select--none">
     <div v-for="(mainItems, mainKey) in menuItems" :key="mainKey">
+      <!-- Main menus -->
       <div
-        class="cursor--pointer d-flex align-center"
+        :ref="getMenuRef(mainItems.url)"
+        class="cursor--pointer"
         @click="routerPush(mainItems.url)"
       >
-        <img
-          class="dark mr-3"
-          width="26"
-          height="26"
-          :src="mainItems.iconDark"
-        />
-        <img
-          class="light mr-3"
-          width="26"
-          height="26"
-          :src="mainItems.iconLight"
-        />
-        <div class="menu-name">{{ mainItems.name }}</div>
-      </div>
-
-      <v-list-item-content
-        v-for="(subItems, subKey) in mainItems.children"
-        :key="subKey"
-      >
-        <div
-          class="menu-sub-item cursor--pointer"
-          @click="routerPush(subItems.url)"
-        >
-          {{ subItems.name }}
+        <div class="d-flex align-center">
+          <img
+            class="dark mr-3"
+            width="26"
+            height="26"
+            :src="mainItems.iconDark"
+          />
+          <img
+            class="light mr-3"
+            width="23"
+            height="23"
+            :src="mainItems.iconLight"
+          />
+          <div class="white--text">{{ mainItems.name }}</div>
         </div>
-      </v-list-item-content>
+        <!-- Sub menus -->
+        <div v-for="(subItems, subKey) in mainItems.children" :key="subKey">
+          <div
+            :ref="getMenuRef(subItems.url)"
+            class="submenu white--text cursor--pointer"
+            @click="
+              routerPush(subItems.url, getMenuRef('parent' + mainItems.url))
+            "
+          >
+            {{ subItems.name }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -71,6 +75,7 @@ export default {
           name: 'Send',
           iconDark: SendDark,
           iconLight: SendLight,
+          url: '/wallet/send/sendtx',
           children: [
             {
               name: 'Send Transaction',
@@ -102,6 +107,7 @@ export default {
           name: 'Contract',
           iconDark: ContractDark,
           iconLight: ContractLight,
+          url: '/wallet/contract/interact',
           children: [
             {
               name: 'Interact with contract',
@@ -122,16 +128,28 @@ export default {
       ]
     };
   },
+  computed: {},
   watch: {
+    /*
     $route(to, from) {
       console.log(to);
       console.log(from);
     }
+    */
   },
   created() {
     this.currentURL = this.$route.path;
+    console.log(this.$refs);
   },
   methods: {
+    getMenuRef(url) {
+      if (url) {
+        return url.replace(/[^\w\s]/gi, '_');
+      }
+      return 'aaa';
+
+      //return sourceString.replace('/', '');
+    },
     routerPush(url) {
       this.currentURL = url;
       this.$router.push({ path: url }, () => {});
@@ -142,4 +160,8 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/styles/GlobalVariables.scss';
+.main-item,
+.sub-item {
+  color: white;
+}
 </style>
